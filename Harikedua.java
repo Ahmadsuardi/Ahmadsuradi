e2e9243673a54ba214b29025c8d589dbd58b0c0e
Ahmadsuardi <146690420+Ahmadsuardi@users.noreply.github.com>
2023-10-10 14:13:03 +0800

    Create Harikedua.java
    Tipe data

---
+++ Harikedua.java
@@ -0,0 +1,24 @@
+
+public class Harikedua {
+    public static void main(String[] args) {
+        String nama = "Ahmadsuardi";
+        String kelas = "Informatika B";
+        System.out.println("nama saya"+nama);
+        System.out.println("kelas"+kelas);
+        
+        //Tipe data
+        byte data1 = 12;
+        short data2 = 15;
+        int data3 = 156;
+        long angkapanjang = 56627L;
+        
+        
+        float data4 = 13.4f;
+        double data5 = 15; //15.0
+        
+        char data6 = 'A'; // hanya menampung satu charakter
+        boolean data7 = true; // dan flse
+
+        
+    }
+}
