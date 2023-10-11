2d711fc037e3466b40de8bef5bfd3f176f03552e
Ahmadsuardi <146690420+Ahmadsuardi@users.noreply.github.com>
2023-10-11 15:53:47 +0800

    Create Hariketiga.java
    Tipe data

---
+++ Hariketiga.java
@@ -0,0 +1,29 @@
+//saya mengulang kodingan kemarin karna belum lancar
+
+class Hariketiga {
+    public static void main(String[] args) {
+        
+        String nama = ("Ahmad Suardi ");
+        System.out.println(nama);
+        String kelas = ("Informatika B");
+        System.out.println(kelas);
+        
+        //Tipe data 
+        
+        byte B.bulat = 12;
+        short B.bulat2 = 15;
+        int B.bulat3 = 156;
+        long angkabanyak = 1255323L;
+        
+        float data1 = 13.4f;
+        double data2 = 17; //17.0
+        
+        char data3 = 'A'// hanya menampung satu charakter
+        boolean data4 = true//false
+    
+        
+        
+        
+        
+    }
+}
