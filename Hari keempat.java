594d7bd407286816d98a881a718d7daab741e439
Ahmadsuardi <146690420+Ahmadsuardi@users.noreply.github.com>
2023-10-12 15:15:33 +0800

    Create Hari keempat.java
    Biodata dan perkenalan

---
+++ Hari keempat.java
@@ -0,0 +1,37 @@
+//Membuat biodata dan perkenalan :
+//Nama
+//Nim
+//Alamat
+//Umur
+//Wa
+//hobi 
+//prodi
+//fakultas
+//Inisial
+
+
+public class HarikeEmpat {
+    public static void main(String[] args) {
+        String Nama = ("Ahmad suardi");
+        String Nim = ("D0223011");
+        String Alamat= ("Renggeang");
+        int Umur = 17;
+        String Wa = ("085311839705");
+        String hoby = ("Sepakbola");
+        String Prodi =("Informatika ");
+        String Fakultas = ("TEKNIK ");
+        char Inisial = 'A';
+
+         System.out.println("perkenalkan nama saya = " + Nama);
+         System.out.println("Dan adapun nim saya = "+Nim);
+         System.out.println("Alamat saya di  =  " + Alamat);
+         System.out.println("Sekarang saya berumur = " + Umur + "tahun");
+         System.out.println("nomor telpon yang saya guanakan sekaligus nomor WA  = " + Wa);
+         System.out.println("saya mempunyai sebuah hoby = " + hoby + "dan prodi saya = "  + Prodi  + "fakultas =  "+ Fakultas);
+         System.out.println("Inisial nama saya = " + 'A');
+         
+         
+         
+        
+    }
+}
