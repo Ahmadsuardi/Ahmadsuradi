5eba4737dcb7db819fe438aa41b0be43fdfc5b0d
Ahmadsuardi <146690420+Ahmadsuardi@users.noreply.github.com>
2023-10-13 18:36:10 +0800

    Create Hari kelima.java
    Menentukan nilai false dan true

---
+++ Hari kelima.java
@@ -0,0 +1,13 @@
+//Menentukan nilai Flase dan true
+
+class HariKelima {
+    public static void main(String[] args) {
+        int nilai = 4;
+        int angka = 7;
+        System.out.println(nilai>angka);
+        //System.out.println(nilai<angka);
+      
+        
+        
+    }
+                           
