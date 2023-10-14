fafdd497e176aa9da13b9ba52083e0269bf7a62d
Ahmadsuardi <146690420+Ahmadsuardi@users.noreply.github.com>
2023-10-14 18:46:14 +0800

    Create Hari keEnam.java

---
+++ Hari keEnam.java
@@ -0,0 +1,10 @@
+//perkalian
+
+class HarikeEnam {
+    public static void main(String[] args) {
+        int angka=2;
+        angka*=6;
+        //jika di run maka yang hasilnya adalah 12 karna 2 * 6 = 12
+     System.out.print(angka);
+    }
+}
