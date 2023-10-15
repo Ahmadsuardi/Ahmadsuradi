8fa79f9dc2729ef8033f83f0cfad760bd7ffc398
Ahmadsuardi <146690420+Ahmadsuardi@users.noreply.github.com>
2023-10-15 19:48:19 +0800

    Create HarikeTuju.java

---
+++ HarikeTuju.java
@@ -0,0 +1,34 @@
+//Scanner biodata 
+import java.util.Scanner;
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        String Nama;
+        String Nim;
+        String Jurusan;
+        System.out.println("input Nama = ");
+        Nama = input.nextLine();
+        System.out.println("input Nim = ");
+        Nim = input.nextLine();
+        System.out.println("input Jurusan = ");
+        Jurusan = input.nextLine();
+        
+        
+    }
+          }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
