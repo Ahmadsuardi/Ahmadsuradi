b3e2ac77ad7203cbfc160c85c10f58a4362bfb70
Ahmadsuardi <146690420+Ahmadsuardi@users.noreply.github.com>
2023-10-16 12:57:35 +0800

    Create Hari keDelapan.java

---
+++ Hari keDelapan.java
@@ -0,0 +1,13 @@
+//Scanner int dan Float
+//bilangan bulat dan bilangan desimal
+
+import java.util.Scanner;
+class HarikeDelapan {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+         System.out.print("Berikan angka bulat =");
+         int AngkaBulat = input.nextInt();
+         System.out.print("angka desimal = ");
+         float Bilangandesimal = input.nextFloat();
+    }
+}
