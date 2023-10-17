f3d5947387e472926298d9455e86a880a07d2f1b
Ahmadsuardi <146690420+Ahmadsuardi@users.noreply.github.com>
2023-10-17 19:02:27 +0800

    Create Hari kesembilan.java
    Operator

---
+++ Hari kesembilan.java
@@ -0,0 +1,15 @@
+//Scanner perkalian
+import java.util.Scanner;
+class HariKeSembilan {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Masukkan angka = ");
+        double angka1 = input.nextDouble();
+        System.out.print("Masukkan angka2 = ");
+        double angka2 = input.nextDouble();
+        System.out.print(angka1 * angka2);
+
+        
+    }
+}
