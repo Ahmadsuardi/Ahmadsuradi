ebdfc492ee019f8a5682fa0bce79c9d9d8a731e6
Ahmadsuardi <146690420+Ahmadsuardi@users.noreply.github.com>
2023-10-20 19:36:59 +0800

    Create Harike12 Operator penugasan.java
    Operator penugasan

---
+++ Harike12 Operator penugasan.java
@@ -0,0 +1,66 @@
+//Operator penugasan 
+
+class HelloWorld {
+    public static void main(String[] args) {
+        // Penugasan (=)
+        int angka;
+          angka = 6;
+        System.out.println(angka);
+       //Jika di Run maka yang di hasilkan adalah 6
+       
+        //penugasan (=+)
+        int a, b;
+        a = 4;
+        b = 7;
+        b += a;
+       System.out.println(b);
+       //Program ini jika di Run maka yang di hasilkan adalah 11
+       
+       //penugasan ( -= )
+       int c, d;
+       c = 6;
+       d = 4;
+       c -= d;
+       System.out.println(c);
+       //Program diatas jika di Run maka yang dihasilkan adalah 2 karna 6 di kurang 4 = 2
+      
+       //Penugasan (/=)
+       int e, f;
+       e = 6;
+       f = 3;
+       e /= f;
+       System.out.println(e);
+       //Program diatas jika di Run akan menghasilkan = 2  karna 6 di bagi 3 = 2
+      
+       
+       //Penugasan (%=)
+       int r, t;
+       r = 8;
+       t = 2;
+       r %= t;
+       System.out.println(r);
+      //Program diatas jika di Run akan menghasilkan = 0 karna tidak ada sisa bagi 
+      
+     //Penugasan (*=)
+      int y, j;
+      y = 5;
+      j = 7;
+      y *= j;
+      System.out.println(y);
+      //Program diatas jika di Run akan menghasilkan = 35  karna 5 dikalikan dengan 7
+          
+
+       
+      
+
+       
+       
+       
+       
+       
+       
+       
+
+       
+    }
+}
