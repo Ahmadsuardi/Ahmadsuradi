6d0bb57eac23ede37adbce149d541008add712f9
Ahmadsuardi <146690420+Ahmadsuardi@users.noreply.github.com>
2023-10-21 22:16:35 +0800

    Create Harike13 Operator perbandingan.java
    Hari ini saya membuat program Operator perbandingan

---
+++ Harike13 Operator perbandingan.java
@@ -0,0 +1,17 @@
+//Operator perbandingan
+class HariKe13 {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 9;
+        System.out.println(a==b);
+        //jika di Run akan manghasilkan false karena kedua variabel tersebut tidak senilai 
+        System.out.println(a!=b);
+        //jika di Run akan menghasilkan true kedua variabel tidak senilai
+        System.out.println(a>=b);
+        //Apabila di Run akan menghasilkan false
+        System.out.println(a<b);
+        //Jika di Run akan menghasilkan true
+        System.out.println(a<=b);
+        //Jika di Run akan menghasilkan true
+    }
+}
