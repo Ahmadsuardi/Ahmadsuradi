6707353fba2a640ce1032671e9e3cad807616fe2
Ahmadsuardi <146690420+Ahmadsuardi@users.noreply.github.com>
2023-10-23 20:05:03 +0800

    Create Harike15lanjutanlogika.java
    Saya melengkapi kodingan operator logika

---
+++ Harike15lanjutanlogika.java
@@ -0,0 +1,56 @@
+class OperatorLogikLanjutanyangkemarin {
+    public static void main(String[] args) {
+        //OPERASI LOGIKA
+        // OR (||)
+        // XOR (^)
+        // NOT (!)
+        
+        System.out.println(" OR (||) ");
+        // jika salah satu variabel bernilai false maka hasilnya adalah false     
+        boolean a,b,c;
+        a = true;
+        b = true;
+        c = a||b;
+        System.out.println(a+"  || "+b+"  = "+c);
+        a = false;
+        b = true;
+        c = a||b;
+        System.out.println(a+" || "+b+"  = "+c);
+        a = true;
+        b = false;
+        c = a||b;
+        System.out.println(a+"  || "+b+" = "+c);
+        a = false;
+        b = false;
+        c = a||b;
+        System.out.println(a+" || "+b+" = "+c);
+        System.out.println(" XOR (^) ");
+        /* XOR diibaratkan perkalian/pembagian dalam operasi matematika true(-)&
+           false(+)*/
+        a = true;
+        b = true;
+        c = a^b;
+        System.out.println(a+"  ^ "+b+"  = "+c);
+        a = false;
+        b = true;
+        c = a^b;
+        System.out.println(a+" ^ "+b+"  = "+c);
+        a = true;
+        b = false;
+        c = a^b;
+        System.out.println(a+"  ^ "+b+" = "+c);
+        a = false;
+        b = false;
+        c = a^b;
+        System.out.println(a+" ^ "+b+" = "+c);
+        System.out.println(" NOT (!) ");
+        // NOT Adalah kebalikan, jika nilainya true maka hasilnya adalah false,begitu juga sebaliknya
+        a = true;
+        c = !a;
+        System.out.println(a+"  --> ! = "+c);
+        a = false;
+        c = !a;
+        System.out.println(a+" --> ! = "+c);
+        
+        
+    } 
