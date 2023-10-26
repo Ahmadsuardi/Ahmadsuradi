51e8d8f9134bd1a7222faddb38fb75ef12c07137
Ahmadsuardi <146690420+Ahmadsuardi@users.noreply.github.com>
2023-10-26 21:12:18 +0800

    Create HariKe18Internary.java

---
+++ HariKe18Internary.java
@@ -0,0 +1,11 @@
+
+public class HariKe18Internary {
+    public static void main(String[] args) {
+       int nilai = 70;
+       int kkm = 65;
+       String hasilTS = nilai >= kkm?"Tuntas":" Tidak tuntas";
+        System.out.println("Nilai Asep : "+nilai);
+        System.out.println("Asep dinyatakan"+hasilTS);
+    } 
+    
+}
