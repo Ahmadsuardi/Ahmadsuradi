827fd990b19880f7476d2434769e5282fb94463c
Ahmadsuardi <146690420+Ahmadsuardi@users.noreply.github.com>
2023-10-27 22:37:17 +0800

    Create HariKe19.Ternary Scanner.java

---
+++ HariKe19.Ternary Scanner.java
@@ -0,0 +1,18 @@
+//Ternary menggunakan Scanner
+import java.util.Scanner;
+
+public class HariKe19{
+
+    public static void main(String[] args) {
+        Scanner  = new Scanner(System.in);
+        System.out.print("Nilai Asep pada saat ujian = ");
+        int nilai = masuk.nextInt();
+        String predikat = 
+                nilai >= 90 ? "Sangat baik" :
+                nilai >= 70 ? "Baik" :
+                nilai >= 50 ? "Cukup" : "Kurang";
+        System.out.println(predikat);
+             
+    } 
+    
+}
