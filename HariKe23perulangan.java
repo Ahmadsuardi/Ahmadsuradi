582980c9d815e74e7b1952e2c2ac021550060f45
Ahmadsuardi <146690420+Ahmadsuardi@users.noreply.github.com>
2023-10-31 20:42:52 +0800

    Create HariKe23perulangan.java

---
+++ HariKe23perulangan.java
@@ -0,0 +1,10 @@
+//Saya belajar perulangan (for)
+//Jika di run akan menghasilkan 2 3 4 5 6 7 8 9 10 
+public class HariKe23Perulangan {
+    public static void main(String[] args) {
+        for(int a = 2; a <= 10; a++){
+        
+        System.out.println(a);
+        }
+    }
+}
