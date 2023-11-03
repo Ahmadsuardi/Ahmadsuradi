f4194dead9ef697ee9dc1d2b35d363af34a956a1
Ahmadsuardi <146690420+Ahmadsuardi@users.noreply.github.com>
2023-11-03 21:20:29 +0800

    Create HariKe26LoopingWhile.java

---
+++ HariKe26LoopingWhile.java
@@ -0,0 +1,16 @@
+//Mencari angka ganjil
+//looping while
+
+public class HariKe26While{
+public static void main(String[] ) {
+        int m = 1;
+        while(m <= 10){
+            int hasil = m % 2;
+            if (hasil == 1){
+                System.out.println(m+" ");
+            }
+            m++;
+            }
+            System.out.println("SELESAI");
+    }
+}
