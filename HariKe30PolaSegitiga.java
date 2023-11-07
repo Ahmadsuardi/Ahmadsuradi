f171b7f2d61aa0f186098b405dc10518c6f09c2e
Ahmadsuardi <146690420+Ahmadsuardi@users.noreply.github.com>
2023-11-07 19:48:28 +0800

    Create HariKe30PolaSegitiga.java

---
+++ HariKe30PolaSegitiga.java
@@ -0,0 +1,12 @@
+public class HeriKe30polaSegitiga {
+public static void main(String[] args) {
+      int jml = 4;
+      for(int i = 1; i <= jml; i++){
+          for(int j = 1; j < i; j++){
+              System.out.print("o ");
+          }
+          System.out.println("o");
+      }
+    }
+    
+}
