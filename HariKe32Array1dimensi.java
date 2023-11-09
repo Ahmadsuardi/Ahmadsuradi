a0b84743580aa0a0a6ae139c755b2a927dd59edc
Ahmadsuardi <146690420+Ahmadsuardi@users.noreply.github.com>
2023-11-09 20:30:12 +0800

    Create HariKe32Array1dimensi.java

---
+++ HariKe32Array1dimensi.java
@@ -0,0 +1,9 @@
+public class HariKe32Array1dimensi { public static void main(String[] args) {
+        int[] a = {4, 7, 9, 10};
+        String[] Nama  = {"Anti","Anto","Ar","Arif"};
+        System.out.println(Nama[1]);
+        //Jika di run maka  keluar adalah Anto
+     
+    }
+      
+}
