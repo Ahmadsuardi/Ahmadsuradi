1527af677a2dda044a9731478657cae34517913d
Ahmadsuardi <146690420+Ahmadsuardi@users.noreply.github.com>
2023-11-10 22:30:01 +0800

    Create HariKe33Arrayfor.java

---
+++ HariKe33Arrayfor.java
@@ -0,0 +1,11 @@
+//Jika program ini di jalankan maka yang muncul adalah semua angka yang ada dalam variabel a
+
+class HeriKe33arrayfor {
+    public static void main(String[] args) {
+        int [] a ={5,7,6,8,9};
+        int semua = a.length;
+        for(int b = 0; b < semua ; b++){
+            System.out.println(a[b]);
+        }
+    }
+}
