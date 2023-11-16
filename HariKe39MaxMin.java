5ecc9f113c3d129a649ccbf669e827142cced808
Ahmadsuardi <146690420+Ahmadsuardi@users.noreply.github.com>
2023-11-16 20:45:50 +0800

    Create HariKe39MaxMin.java

---
+++ HariKe39MaxMin.java
@@ -0,0 +1,21 @@
+public class HariKe39MaxMin {
+public static void main(String[] args) {
+      //Menentukan Max dan Min
+      int []a ={1,2,3,4,5,6,7,8};
+      int jml_index = a.length;
+      int max = a[0],  = a[0];
+        System.out.println("Deret bil : ");
+        for (int i = 0; i < jml_index; i++) {
+            System.out.print(a[i]+" ");
+        if(a[i] > max){
+                max = a[i];             
+        }
+        if(a[i] < min){    
+        }
+       
+        }
+            System.out.println("");
+            System.out.println("max : "+max);
+            System.out.println("min : "+min);
+       }
+    }
