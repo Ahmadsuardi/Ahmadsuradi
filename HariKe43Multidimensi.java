c2ca162a6b5ccdd14bb062a10d82c0d0b155033d
Ahmadsuardi <146690420+Ahmadsuardi@users.noreply.github.com>
2023-11-20 20:31:34 +0800

    Create HariKe43Multidimensi.java

---
+++ HariKe43Multidimensi.java
@@ -0,0 +1,13 @@
+public class hariKe43Multidimensi {
+    public static void main(String[] args) {
+       String[][] Alamat = {
+           {"Alex","\t (Lembang)"},
+           {"Firman","\t (Majene)"}
+       };
+        System.out.print(Alamat[0][0]);
+        System.out.println(Alamat[0][1]);
+        System.out.print(Alamat[1][0]);
+        System.out.println(Alamat[1][1]);
+    }
+    
+                           
