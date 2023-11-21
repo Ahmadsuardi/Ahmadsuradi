93c29cca80b45a3496ba17917fc9d6084c264fe0
Ahmadsuardi <146690420+Ahmadsuardi@users.noreply.github.com>
2023-11-21 20:16:15 +0800

    Create HariKe44IndexMul.java

---
+++ HariKe44IndexMul.java
@@ -0,0 +1,13 @@
+public class HariKe44index {
+    public  void main(String[] args) {
+        String[][] kontak = {
+            {"Farhan", " (*888#)"},
+            {"Albih", " (*808#)"},
+            {"Upin", " (*888*70#)"}
+        };
+        for (int i = 0; i < kontak.length; i++) {
+            System.out.print(kontak[i][0]);
+            System.out.println(kontak[i][1]);
+        }
+    }
+}
