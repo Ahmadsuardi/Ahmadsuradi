dedafe1e1be3fbe143a5e3d283167bc96b3e590a
Ahmadsuardi <146690420+Ahmadsuardi@users.noreply.github.com>
2023-11-23 21:52:28 +0800

    Create HariKe46LibraryArray.java

---
+++ HariKe46LibraryArray.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.Collections;
+public class HariKe46Library {
+    public static void main(String[] args) {
+        Integer[] a = {2,3,33,5,6,7};
+        int maks = Collections.max(Arrays.asList(a));
+        int min = Collections.min(Arrays.asList(a));
+        System.out.println(maks);
+        System.out.println(min);
+    }
+}
