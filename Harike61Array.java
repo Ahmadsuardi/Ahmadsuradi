382511bdf0e95991923d549eac3d824e63cac349
Ahmadsuardi <146690420+Ahmadsuardi@users.noreply.github.com>
2023-12-08 21:24:44 +0800

    Create Harike61Array.java

---
+++ Harike61Array.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+public class Hari61Array{
+    public static void main(String[] args) {
+        String[] Goat = {"Messi","Ronaldo","Maguire"};
+        Goat[2] = "Ramos";
+        System.out.println(Arrays.toString(Goat));
+    }
+    
+}
