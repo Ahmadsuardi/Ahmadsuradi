64175b5eac0e4d9a9af926172696588395ab4b19
Ahmadsuardi <146690420+Ahmadsuardi@users.noreply.github.com>
2023-12-09 22:43:10 +0800

    Create HariKe62menggunakanWhile.java

---
+++ HariKe62menggunakanWhile.java
@@ -0,0 +1,10 @@
+public class HariKe61menggunakanWhile {
+ public static void main(String[] args) {
+     int[]numbers = {10,20,30,40,50,};
+        System.out.println("Menggunakan while :");
+        int j = 0;
+        while(j < numbers.length){
+            System.out.println(numbers[j]);
+            j++;
+        }               
+    }   
