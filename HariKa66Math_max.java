cb294f0183a13308b81bd1f14e2384cf5afd006f
Ahmadsuardi <146690420+Ahmadsuardi@users.noreply.github.com>
2023-12-13 21:14:19 +0800

    Create HariKa66Math_max.java

---
+++ HariKa66Math_max.java
@@ -0,0 +1,8 @@
+public class HariKe66Math_max {
+    public static void main(String[] args) {
+       double x = 100;
+       double y = 99;
+       double z = Math.max(x, y);
+        System.out.println(z);
+    }
+}
