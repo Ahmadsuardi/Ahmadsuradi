0659de806138c111f8b8f527e420057685710b6f
Ahmadsuardi <146690420+Ahmadsuardi@users.noreply.github.com>
2023-12-14 22:25:44 +0800

    Create Harike67Math_sqrt.java

---
+++ Harike67Math_sqrt.java
@@ -0,0 +1,10 @@
+public class HariKe67Math_sqrt {
+    public static void main(String[] args) {
+        //Akar dari 100 ada 10
+        //Karna tipe data yang digunakan itu double maka hasilnya adalah 10.0
+       double x = 100;
+       double z = Math.sqrt(x);
+       System.out.print("Hasil : ");
+        System.out.println(z);
+    }
+}
