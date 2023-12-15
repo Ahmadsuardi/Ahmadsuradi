b537e2b8b84050f1533560e13ff8c8722bbd465f
Ahmadsuardi <146690420+Ahmadsuardi@users.noreply.github.com>
2023-12-15 21:52:04 +0800

    Create Harike68Math_pow.java

---
+++ Harike68Math_pow.java
@@ -0,0 +1,9 @@
+public class HariKe68Math_pow {
+    public static void main(String[] args) {
+       //2 pangkat 2 maka hasilnya adalah 4
+      double x = 2;
+      double z = Math.pow(x,2);
+       System.out.print("Hasil : ");
+        System.out.println(z);
+    }
+}
