686d6d3fc16582fb529e0329a575654252d48b0a
Ahmadsuardi <146690420+Ahmadsuardi@users.noreply.github.com>
2023-12-17 21:46:47 +0800

    Create Hari70_Math_Penjumlahan.java

---
+++ Hari70_Math_Penjumlahan.java
@@ -0,0 +1,10 @@
+public class HariKe70Math_Jumlah {
+    public static void main(String[] args) {
+     int x = 30;
+     int y = 20;
+      
+     int v = Math.addExact(x, y);
+        System.out.print("Hasil :");
+        System.out.println(v);
+    }    
+}
