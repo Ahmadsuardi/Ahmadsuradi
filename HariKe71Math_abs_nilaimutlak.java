29dcd9c9a670631e3f622e9cbbc1c8020ab67b91
Ahmadsuardi <146690420+Ahmadsuardi@users.noreply.github.com>
2023-12-18 22:33:29 +0800

    Create HariKe71Math_abs_nilaimutlak.java

---
+++ HariKe71Math_abs_nilaimutlak.java
@@ -0,0 +1,7 @@
+public class HariKe71Math_abs_nilaimutlak {
+    public static void main(String[] args) {
+       double x = 100;
+       double z = Math.abs(x);
+        System.out.println(z);
+    }
+}
