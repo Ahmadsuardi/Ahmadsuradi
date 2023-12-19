e6a09a0b74baded965881a0ef07bd8f927f6755a
Ahmadsuardi <146690420+Ahmadsuardi@users.noreply.github.com>
2023-12-19 21:23:48 +0800

    Create Harike72Math.ceil.java

---
+++ Harike72Math.ceil.java
@@ -0,0 +1,8 @@
+public class Harike72Mathceil_pembulatanKeatas {
+//Math.ceil pembulatan keatas
+    public static void main(String[] args) {
+        double angka = 17.75;
+        double hasilpembulatan = Math.ceil(angka);
+        System.out.println("Angka setelah dibulatkan ke atas : "+hasilpembulatan);
+    }   
+}
