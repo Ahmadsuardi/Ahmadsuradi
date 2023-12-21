902d2f0c0140f49fb63fa8d78a34e5a19c8c1473
Ahmadsuardi <146690420+Ahmadsuardi@users.noreply.github.com>
2023-12-21 21:22:58 +0800

    Create HariKe74_Random_for.java

---
+++ HariKe74_Random_for.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+public class Harike74Rando_lop {
+public static void main(String[] args) {
+       Random acak = new Random();
+       double angka;
+       for(int i = 0; i < 5; i++){
+           angka = acak.nextInt(100);
+           System.out.println(angka);
+       }
+    }
+}    
