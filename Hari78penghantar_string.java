96df6b8627e2b11f1fdf239afc25361ab2c89f79
Ahmadsuardi <146690420+Ahmadsuardi@users.noreply.github.com>
2023-12-24 21:59:46 +0800

    Create Hari78penghantar_string.java

---
+++ Hari78penghantar_string.java
@@ -0,0 +1,8 @@
+public class HariKe78PenghantarString {
+    public static void main(String[] args) {
+      String nama = "Ahmad suardi";
+      System.out.println("jml karakter\t: "+nama.length());
+      System.out.println("uppercase\t: "+nama.toUpperCase());
+      System.out.println("jml karakter\t: "+nama.toLowerCase());
+    } 
+}
