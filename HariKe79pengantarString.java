4eeb80fa26fb56d9b349bb5597d8a0423514b3a5
Ahmadsuardi <146690420+Ahmadsuardi@users.noreply.github.com>
2023-12-25 19:05:30 +0800

    Create HariKe79pengantarString.java

---
+++ HariKe79pengantarString.java
@@ -0,0 +1,15 @@
+public class HariKe79PengantarString {
+public static void main(String[] args) {
+      String nama = "Ahmad Suardi";
+      System.out.println("jml karakter\t: "+nama.length());
+      System.out.println("uppercase\t: "+nama.toUpperCase());
+      System.out.println("jml karakter\t: "+nama.toLowerCase());
+     
+      
+        System.out.println("char[1]\t\t: "+nama.charAt(0));
+        String[] kata = nama.split(" ");
+        System.out.println("nama depan\t: "+kata[0]);
+        System.out.println("nama belakang\t: "+kata[1]);
+        System.out.println("jml kata\t: "+kata.length);
+    } 
+}
