e1051d08bb2bdaa6d28a3096f9ebd4e867bdaac9
Ahmadsuardi <146690420+Ahmadsuardi@users.noreply.github.com>
2023-12-28 18:45:13 +0800

    Create HariKe81ReverString.java

---
+++ HariKe81ReverString.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class ReverPrintString {
+public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan Kata : ");
+        String word = sc.nextLine();
+        for (int i = word.length() - 1; i >= 0; i--) {
+            System.out.print(word.charAt(i) + " ");
+        }
+        System.out.println();
+    }
+}
