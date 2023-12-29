0657b674cc99a34575cc3b3e1c927a6cc21c9816
Ahmadsuardi <146690420+Ahmadsuardi@users.noreply.github.com>
2023-12-29 17:22:23 +0800

    Create HariKe82ArraySort.java

---
+++ HariKe82ArraySort.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class array {
+public static void main(String[] args) {
+  int [] array = { 65, 80, 74, 89 } ;
+  int Max = array[0];
+  
+  Arrays.sort(array);
+  for (int i = 0; i < array.length - 1; i++){
+    if( array[i] > Max ){
+      Max = array[i];
+    }
+  }
+   System.out.println(Max);
+  }
+}
