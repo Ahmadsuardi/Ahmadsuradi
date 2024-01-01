b20b22360da088d1fd490a1ac8803a879869d1d2
Ahmadsuardi <146690420+Ahmadsuardi@users.noreply.github.com>
2024-01-01 20:44:50 +0800

    Update HariKe85_Math_floor.java

--- HariKe85_Math_floor.java
+++ HariKe85_Math_floor.java
@@ -1,7 +1,7 @@
 public class HariKe85Math_floor {
     public static void main(String[] args) {
        int a;
-       a = (int) Math.floor(5.70);
+       a = (int) Math.floor(7.5);
         System.out.println(a);
     }
 }
