3e4a2be5076c6bc99eec9ef4e443df6d69436c2c
Ahmadsuardi <146690420+Ahmadsuardi@users.noreply.github.com>
2024-01-04 21:13:44 +0800

    Create HariKe88FormatString.java

---
+++ HariKe88FormatString.java
@@ -0,0 +1,13 @@
+public class HariKe88formatString {
+public static void main(String[] args) {
+        
+        String nama = "Udin";
+        int umur = 17;
+       
+        // output : Nama saya adalah Udin, umur saya adalah 17
+        System.out.println("Nama saya adalah " + nama + ", umur saya adalah " + umur);
+        
+        // cara lainnya adalah dengan format string
+         System.out.printf("Nama saya adalah %s, umur saya adalah %d" ,nama,umur);
+    }    
+}
