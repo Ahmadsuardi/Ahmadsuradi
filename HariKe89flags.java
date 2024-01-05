ba463fe5e3b632ead19d22cf3512401bf66556ec
Ahmadsuardi <146690420+Ahmadsuardi@users.noreply.github.com>
2024-01-05 22:03:35 +0800

    Create HariKe89flags.java

---
+++ HariKe89flags.java
@@ -0,0 +1,10 @@
+public class HariKe89flags {
+public static void main(String[] args) {
+        // [flags]
+        System.out.println("\n[flags]");
+        int int1 = 5;
+        int int2 = 8;
+        int hasil = int1 + int2;
+        System.out.printf("%d + %d = %+d",int1,int2,hasil);
+    }
+}
