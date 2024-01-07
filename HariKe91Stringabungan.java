07c670752ec60eefa896c4c4e316994cb1f1ab26
Ahmadsuardi <146690420+Ahmadsuardi@users.noreply.github.com>
2024-01-07 21:46:17 +0800

    Create HariKe91Stringabungan.java

---
+++ HariKe91Stringabungan.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.lang.String;
+public class HariKe91StringGabungan {
+public static void main(String[] args) {
+        String kalimat = "aku menyukai si A";
+        
+        System.out.println(kalimat.charAt(16));
+      
+        String kata = kalimat.substring(3,12);
+        System.out.println(kata);
+        
+        String kalimat2 = kata + " Patima";
+        System.out.println(kalimat2);
+    }
+    
+}
