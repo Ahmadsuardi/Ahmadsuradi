e6f43ce5fa056bcb580e6dc03c40c4c97c440933
Ahmadsuardi <146690420+Ahmadsuardi@users.noreply.github.com>
2024-01-08 22:04:11 +0800

    Create HariKe92UpperCase_LowerCase.java

---
+++ HariKe92UpperCase_LowerCase.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.lang.String;
+public class HariKe92UpperCase_LowerCase {
+public static void main(String[] args) {
+        String kalimat = "UntuK mengubah huruf kapital dan huruf kecil ";
+        //Untuk to.UpperCase adalah mengubah huruf kecil menjadi huruh kapital sedang huruf kapital ke huruf kecil itu adalah toLowerCase
+        
+        System.out.println(kalimat.toUpperCase());
+        System.out.println(kalimat.toLowerCase());
+    }
+ } 
