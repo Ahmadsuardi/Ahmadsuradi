5b7bb50ba4352654f0ef23b39722f52485ea69f6
Ahmadsuardi <146690420+Ahmadsuardi@users.noreply.github.com>
2024-01-09 23:42:34 +0800

    Create HariKe93copare.java

---
+++ HariKe93copare.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.lang.String;
+public class HariKe93compare {
+public static void main(String[] args) {
+        String kalimat = "bakwan";
+        String kata = "bala-bala";
+        System.out.println(kalimat.compareTo(kata));
+        System.out.println(kata.compareTo(kalimat));
+    }
+}
